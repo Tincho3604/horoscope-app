c26acd6211b0098b079b3a3063e4ae1ee66bbf78
Tincho3604 <tincho3604@gmail.com>
2021-04-06 12:49:57 -0300

    refactor: add assets and configure .eslintrc.js

---
+++ src/Components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View, Image} from 'react-native';
+//import Dama from '../../Material/index';
+
+const Home = () => {
+  return (
+    <>
+      <Image source={require('../../Assets/boton_leo.png')} />
+    </>
+  );
+};
+
+export default Home;
